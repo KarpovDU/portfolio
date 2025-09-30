6d5a891461af02752b3438388a2cb46929982619
KarpovDU <karpovdu.01@gmail.com>
2025-09-30 21:05:57 +0300

    feat: spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react"
 import styles from "./app.module.scss"
-import { Navigation } from "./components"
+import { Navigation, Spinner } from "./components"
 import { Biography, Footer, Libraries, Projects, Technologies, Work } from "./sections"
 
 export const App = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false)
+    }
+    if (document.readyState === "complete") {
+      setLoading(false)
+    } else {
+      window.addEventListener("load", handleLoad)
+      return () => window.removeEventListener("load", handleLoad)
+    }
+  }, [])
+
+  if (loading) return <Spinner />
+
   return (
     <div className={styles.background}>
       <div className={styles.content}>
