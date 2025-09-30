e521fa8f37435c9c39e2d57f0f5c4cc70a1fae89
KarpovDU <karpovdu.01@gmail.com>
2025-10-01 00:43:34 +0500

    fix: resources loading

--- src/components/spinner/index.tsx
+++ src/components/spinner/index.tsx
@@ -1,5 +1,7 @@
 import { ImSpinner2 } from "react-icons/im"
 import styles from "./index.module.scss"
+import { useEffect, useState, type ReactNode } from "react"
+import FontFaceObserver from "fontfaceobserver"
 
 export const Spinner = () => {
   return (
@@ -7,4 +9,42 @@ export const Spinner = () => {
       <ImSpinner2 className={styles.spinner} />
     </div>
   )
+}
+
+export const Preloader = ({ children }: { children: ReactNode }) => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let imagesLoaded = 0
+    let fontsLoaded = false
+
+    const images = Array.from(document.images)
+    const checkImages = () => {
+      imagesLoaded++
+      if (imagesLoaded === images.length && fontsLoaded) {
+        setLoading(false)
+      }
+    }
+
+    images.forEach(img => {
+      if (img.complete) checkImages()
+      else img.addEventListener("load", checkImages)
+    })
+
+    const font = new FontFaceObserver("PatrickHand")
+    font.load().then(() => {
+      fontsLoaded = true
+      if (imagesLoaded === images.length) setLoading(false)
+    })
+
+    return () => {
+      images.forEach(img => img.removeEventListener("load", checkImages))
+    }
+  }, [])
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  return <>{children}</>
 }
